ff0753d076371a04c647f09ab4ae13701c18af84
flori4nh@gmail.com <flori4nh@gmail.com>
2024-10-24 11:56:52 +0200

    Working 10k, feat: trim output for large vectors

--- ex02/a.cpp
+++ ex02/a.cpp
@@ -6,6 +6,29 @@
 #include <ctime>
 #include <algorithm>
 
+// printing
+void print_vec(const std::vector<int> &vec_semi_sorted) 
+{
+    const int vector_length = vec_semi_sorted.size();
+    for (size_t i = 0; i < vector_length; i++){
+        if (i % 2 == 0)
+            std::cout << std::setw(2) << vec_semi_sorted.at(i) << ' ';
+    }
+    std::cout << '\n';
+    for (size_t i = 0; i < vector_length; i++){
+        if (i % 2 == 1)
+            std::cout << " | " ;
+    }
+    std::cout << '\n';
+
+    for (size_t i = 0; i < vector_length; i++){
+        if (i % 2 == 1)
+            std::cout << std::setw(2) << vec_semi_sorted.at(i) << ' ';
+    }
+    std::cout << "\n\n";
+
+}
+
 int randint(int lower, int higher)
 {
     int random_value = (std::rand() % (higher - lower)) + lower;
@@ -14,15 +37,20 @@ int randint(int lower, int higher)
 
 std::ostream& operator<<(std::ostream &os, const std::vector<int> v)
 {
+    int count = 0;
     for (const int i : v) {
-        os << i << ", ";
+        if (count < 20)
+            os << i << ", ";
+        count += 1;
     }
+    if (count >= 20)
+        os << " ... " << "(" << v.size() << " total)";
     return os;
 }
 
 void binary_insert(const int num, int range, std::vector<int>& v){
 
-    int p = std::pow(2, std::ceil( std::log2(range + 0.01)));
+    const int p = std::pow(2, std::ceil( std::log2(range + 0.01)));
     const int v_len = v.size();
 
     int step;
@@ -46,17 +74,12 @@ void binary_insert(const int num, int range, std::vector<int>& v){
     else
         pos += 1;
 
-
-
-    
-
-
     v.insert(std::min(v.begin() + pos, v.end()), num);
 }
 
 int main(int argc, char const *argv[])
 {
-    const int vector_length = 300;
+    const int vector_length = 10000;
     std::srand(std::time(nullptr));
     // std::srand(5);
     
@@ -70,28 +93,7 @@ int main(int argc, char const *argv[])
     }
     std::cout << vec_semi_sorted << '\n' << std::endl;
 
-    // printing
-    {
-        std::cout << std::setw(2);
-
-        for (size_t i = 0; i < vector_length; i++){
-            if (i % 2 == 0)
-                std::cout << std::setw(2) << vec_semi_sorted.at(i) << ' ';
-        }
-        std::cout << '\n';
-        for (size_t i = 0; i < vector_length; i++){
-            if (i % 2 == 1)
-                std::cout << " | " ;
-        }
-        std::cout << '\n';
-
-        for (size_t i = 0; i < vector_length; i++){
-            if (i % 2 == 1)
-                std::cout << std::setw(2) << vec_semi_sorted.at(i) << ' ';
-        }
-        std::cout << "\n\n";
-
-    }
+    
 
 
     std::vector<int> vec;
