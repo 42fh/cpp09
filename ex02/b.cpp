8dc7928145f295e263ffd6dd31442816b186bbdb
fheld <fheld@student.42berlin.de>
2024-10-25 22:03:25 +0200

    feature: x interpretation and insertion in x coordinates

--- ex02/b.cpp
+++ ex02/b.cpp
@@ -16,12 +16,17 @@ public:
 	block_vector(const t_iv &other, const unsigned int &block_size_initializer);
 	~block_vector();
 	
-
 	const unsigned int block_size;
 
-	t_iv getA(const unsigned int i) const;
-	t_iv getB(const unsigned int i) const;
+	void insert_X_block(const unsigned int pos, const t_iv &block_vec);
 
+	t_iv get_A_block(const unsigned int i) const;
+	t_iv get_B_block(const unsigned int i) const;
+	t_iv get_X_block(const unsigned int i) const;
+
+	int get_A(const unsigned int i) const;
+	int get_B(const unsigned int i) const;
+	int get_X(const unsigned int i) const;
 
 	const t_iv& getVector() const {
 		return _vector;
@@ -46,8 +51,35 @@ block_vector::~block_vector()
 {
 }
 
+void block_vector::insert_X_block(const unsigned int pos, const t_iv &block_vec)
+{
+	if (pos == 0 || pos > _vector.size() / block_size)
+	{
+		std::cerr << "insert_X_block tried inserting at " << pos << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
+		throw std::exception();
+	}
+
+	_vector.insert(_vector.begin() + (pos - 1) * block_size, block_vec.begin(), block_vec.end());
+}
+
+int block_vector::get_A(const unsigned int i) const
+{
+	return _vector.at(2 * (i - 1) * block_size);
+}
+
+int block_vector::get_B(const unsigned int i) const
+{
+	return _vector.at(2 * (i - 1) * block_size + block_size);
+}
+
+int block_vector::get_X(const unsigned int i) const
+{
+	return _vector.at((i - 1) * block_size);
+}
+
+
 // one indexed acess for block A_1, A_2, ...
-t_iv block_vector::getA(const unsigned int i) const
+t_iv block_vector::get_A_block(const unsigned int i) const
 {
 	if (_vector.size() < 2 * (i - 1) * block_size + block_size)
 	{
@@ -67,16 +99,16 @@ t_iv block_vector::getA(const unsigned int i) const
 }
 
 // one indexed acess for block B_1, B_2, ...
-t_iv block_vector::getB(const unsigned int i) const
+t_iv block_vector::get_B_block(const unsigned int i) const
 {
 	if (_vector.size() < 2 * (i) * block_size)
 	{
-		std::cerr << "getB tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
+		std::cerr << "get_B_block tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
 		throw std::exception();
 	}
 	if (i == 0)
 	{
-		std::cerr << "getA is 1 indexed (0 is invalid) tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
+		std::cerr << "get_A_block is 1 indexed (0 is invalid) tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
 		throw std::exception();
 	}
 
@@ -85,6 +117,27 @@ t_iv block_vector::getB(const unsigned int i) const
 	block.insert(block.begin(), block_start, block_start + block_size);
 	return block;
 }
+
+// one indexed acess for block B_1, B_2, ...
+t_iv block_vector::get_X_block(const unsigned int i) const
+{
+	if (_vector.size() < (i) * block_size)
+	{
+		std::cerr << "get_B_block tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
+		throw std::exception();
+	}
+	if (i == 0)
+	{
+		std::cerr << "get_A_block is 1 indexed (0 is invalid) tried accessing element " << i << " for blocksize " << block_size << " and vector size " << _vector.size() << " \n";
+		throw std::exception();
+	}
+
+	t_iv block;
+	const t_iv::const_iterator block_start = _vector.begin() + (i - 1) * block_size; 
+	block.insert(block.begin(), block_start, block_start + block_size);
+	return block;
+}
+
 
 // ----
 
@@ -111,17 +164,55 @@ int main(int argc, char const *argv[])
 	OS << one_swap_vector << "\n\n";
 
 
-	// -----
+	// block_vector demo
+	/*
+	{
+		block_vector bv1(one_swap_vector, 4);
+		OS << "block vect \n " << bv1.getVector(); 
+		OS << "block A_1 = \n " << bv1.get_A_block(1); 
+		OS << "block B_1 = \n " << bv1.get_B_block(1); 
+		OS << "block A_2 = \n " << bv1.get_A_block(2); 
+		OS << "block B_2 = \n " << bv1.get_B_block(2); 
 
-	block_vector bv1(one_swap_vector, 4);
-	OS << "block vect \n " << bv1.getVector(); 
-	OS << "block A_1 = \n " << bv1.getA(1); 
-	OS << "block B_1 = \n " << bv1.getB(1); 
-	OS << "block A_2 = \n " << bv1.getA(2); 
-	OS << "block B_2 = \n " << bv1.getB(2); 
+		block_vector bv2(one_swap_vector, 16);
+		OS << "block A_1 = \n " << bv2.get_A_block(1); 
+	}
+	*/
+
+
+	block_vector four_blocks(one_swap_vector, 4); // four blocks of size four
+
+	t_iv four_blocks_sorted;
+
+	// blind chain
+	t_iv a2 = four_blocks.get_A_block(2);	four_blocks_sorted.insert(four_blocks_sorted.begin(), a2.begin(), a2.end());
+	t_iv a1 = four_blocks.get_A_block(1);	four_blocks_sorted.insert(four_blocks_sorted.begin(), a1.begin(), a1.end());
+	t_iv b1 = four_blocks.get_B_block(1);	four_blocks_sorted.insert(four_blocks_sorted.begin(), b1.begin(), b1.end());
+
+	// insert b2
+	t_iv b2 = four_blocks.get_B_block(2);
+
+	block_vector four_blocks_sorted_BV(four_blocks_sorted, 4);
+	unsigned int pos = 2;
+	if (four_blocks_sorted_BV.get_X(pos) > b2.front())
+		pos -= 1;
+	else
+		pos += 1;
+	
+	if (four_blocks_sorted_BV.get_X(pos) > b2.front())
+		pos += 0;
+	else
+		pos += 1;
+	
+	OS << "pos = " << pos << EL;
+	four_blocks_sorted_BV.insert_X_block(pos, b2);
+	// four_blocks_sorted.insert(four_blocks_sorted.begin() + (pos - 1) * 4, b2.begin(), b2.end());
 
 
 
+
+	OS << four_blocks_sorted << EL;
+	OS << four_blocks_sorted_BV.getVector() << EL;
 
 
 	return 0;
