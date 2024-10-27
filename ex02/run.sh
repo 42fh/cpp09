73c028188b58f24cefe6650ab580ec8a58d449c8
fheld <fheld@student.42berlin.de>
2024-10-27 19:24:53 +0100

    feat: reproduciable random seqences in shell

--- ex02/run.sh
+++ ex02/run.sh
@@ -1,1 +1,13 @@
-./PmergeMe $(seq 11 | sort -R | tr '\n' ' ')
+#! /bin/bash
+
+get_seeded_random()
+{
+  seed="$1"
+  openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt \
+    </dev/zero 2>/dev/null
+}
+
+SEED=$RANDOM
+echo "SEED = $SEED"
+
+./PmergeMe $(seq 100 | sort --random-source=<(get_seeded_random $SEED) -R  | tr '\n' ' ')
