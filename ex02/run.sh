a8452838d14f052085520df902bd33af599f9ede
flori4nh@gmail.com <flori4nh@gmail.com>
2024-10-28 16:17:07 +0100

    bug: NUM=7 SEED=24253

--- ex02/run.sh
+++ ex02/run.sh
@@ -7,8 +7,28 @@ get_seeded_random()
     </dev/zero 2>/dev/null
 }
 
-# SEED=$RANDOM
-SEED='55690'
-echo "SEED = $SEED"
+SEED=$RANDOM
+# NUM=$((1 + $RANDOM % 100))
 
-./PmergeMe $(seq 9 | gsort --random-source=<(get_seeded_random $SEED) -R  | tr '\n' ' ')
+NUM='29'
+
+# SEED="17931"
+# NUM="61"
+
+# SEED='10314'
+NUM='15'
+
+# SEED="3413"
+# NUM= "15"
+
+NUM='7'
+SEED= "24253"
+
+
+
+echo "SEED = $SEED"
+echo "NUM = $NUM"
+
+
+
+./PmergeMe $(seq $NUM | gsort --random-source=<(get_seeded_random $SEED) -R  | tr '\n' ' ')
